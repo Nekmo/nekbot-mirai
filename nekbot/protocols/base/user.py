82a1c811f87ba2a6d5d09bb59159228340c0eb05
Nekmo <contacto@nekmo.com>
2015-04-26 00:57:22 +0200

    Soporte para Users (listados de usuarios) y asociación de usuario a sala para protocolos como Jabber.

--- nekbot/protocols/base/user.py
+++ nekbot/protocols/base/user.py
@@ -1,9 +1,11 @@
+from collections import defaultdict
+
 __author__ = 'nekmo'
 
 class User(object):
 
-    def __init__(self, protocol, username, id=None):
-        self.protocol, self.username, self.id = protocol, username, id
+    def __init__(self, protocol, username, id=None, groupchat=None):
+        self.protocol, self.username, self.id, self.groupchat = protocol, username, id, groupchat
 
     def send_message(self, body, notice=False):
         raise NotImplementedError("This protocol can't send messages to users.")
@@ -13,3 +15,7 @@ class User(object):
 
     def __str__(self):
         return self.username
+
+class Users(dict):
+    def __init__(self, protocol):
+        dict.__init__(self)
