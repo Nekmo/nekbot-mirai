d847146b1659a59dcd0bd3fd34d47b91cc62bffe
Nekmo <contacto@nekmo.com>
2015-04-29 20:43:22 +0200

    [default] [Test] Cambiada línea para dependencias por problema entre versiones 6.06 y 6.1.1 de Pip.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ except IOError:
 __dir__ = os.path.abspath(os.path.dirname(__file__))
 
 requirements = parse_requirements('requirements.txt', session=uuid.uuid1())
-install_requires = [str(ir.req) for ir in requirements if ir.link and not ir.link.url]
+install_requires = [str(ir.req) for ir in requirements if getattr(ir, 'url', ir.url.link)]
 
 packages = find_packages(__dir__)
 # Prevent include symbolic links
