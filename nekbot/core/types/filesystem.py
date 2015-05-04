b98745fbf2dd77a06a63a8e0c089a4e2d7f9eb24
Nekmo <contacto@nekmo.com>
2015-05-04 22:19:08 +0200

    [default] [Test] Mixer para Nodes: Permitir múltiples roots para la búsqueda del nodo.

--- nekbot/core/types/filesystem.py
+++ nekbot/core/types/filesystem.py
@@ -17,10 +17,17 @@ class Node(object):
     def additional_checks(self, node):
         return node
 
-    def __call__(self, node):
-        node = os.path.abspath(os.path.join(self.root, node))
-        if not node.startswith(self.root):
+    def path_security(self, node, root):
+        if not node.startswith(root):
             raise SecurityError('The path does not belong to the base path.', node)
+        return node
+
+    def get_path(self, node):
+        node =  os.path.abspath(os.path.join(self.root, node))
+        return self.path_security(node, self.root)
+
+    def __call__(self, node):
+        node = self.get_path(node)
         if not os.path.exists(node):
             return self.path_not_exist(node)
         return self.additional_checks(node)
@@ -38,3 +45,21 @@ class Dir(Node):
         if not os.path.isdir(node):
             raise InvalidArgument('The path is not a directory.', node)
         return node
+
+
+class MultiplesRootsMix(object):
+    def __init__(self, *roots):
+        for root in roots:
+            root = os.path.abspath(root)
+            if not os.path.exists(root):
+                raise ValueError('Invalid root for %s: "%s"' % (self.__class__.__name__,
+                                                                root))
+        self.roots = roots
+
+    def get_path(self, node):
+        for root in self.roots:
+            path = os.path.abspath(os.path.join(root, node))
+            if not os.path.exists(path):
+                continue
+            return self.path_security(path, root)
+        self.path_not_exist(node)
